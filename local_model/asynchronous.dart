b2e227f58958b86c62be586850412b60a29e101b
mahamudulhasan12 <mahamudulhasanmunna459@gmail.com>
2026-08-25 00:12:37 +0600

    json Data

--- local_model/asynchronous.dart
+++ local_model/asynchronous.dart
@@ -19,19 +19,24 @@
 // }
 
 
+
+
+
+import '../list.dart';
 import 'dummy_json.dart';
 import 'model.dart';
 
 void main(){
-  List<StudentModel> studentList = DummyJson.dummyData.map((json)=>StudentModel.fromJsonData(json)).toList();
-  for(var Student in studentList){
-    print(Student.name);
-  }
+  getData();
 }
-// void pFun()async{
-//   print("Dart Programming");
-//   await Future.delayed(Duration(seconds :5));
-//   StudentModel student = StudentModel.fromJsonData(rawData);
 
-// }
+void getData()async{
+  List<StudentModel> sData = DummyJson.dummyData.map((json)=>StudentModel.fromJsonData(json)).toList();
+  for(int i =0; i<=sData.length;i++){
+    if(i == 0){
+      print(sData[0].name);
+    }
+  }
+
+}
 
--- local_model/model.dart
+++ local_model/model.dart
@@ -32,20 +32,38 @@ class StudentModel{
       this.institution
       
   });
-  StudentModel.fromJsonData(Map<String,dynamic>json){
-   studentId = json["studentId"];
-   name = json['name'];
-   roll = json['roll'];
-   registrationNo = json['registrationNo'];
-   department = json['department'];
+  // StudentModel.fromJsonData(Map<String,dynamic>json){
+  //  studentId = json["studentId"];
+  //  name = json['name'];
+  //  roll = json['roll'];
+  //  registrationNo = json['registrationNo'];
+  //  department = json['department'];
+  // }
+  // Map<String , dynamic> toJson(){
+  //   final Map<String , dynamic> data = new Map<String , dynamic>();
+  //   data["studentId"] = this.studentId;
+  //   data['name'] = this.name;
+  //   data['roll'] = this.roll;
+  //   data['registrationNo'] = this.registrationNo;
+  //   data['department'] = this.department;
+  //   return data;
+  // }
+
+  StudentModel.fromJsonData(Map<String , dynamic > json){
+    studentId = json["StudentId"];
+    name = json['name'];
+    roll = json['roll'];
+    department = json['department'];
+    institution = json['institution'];
   }
-  Map<String , dynamic> toJson(){
-    final Map<String , dynamic> data = new Map<String , dynamic>();
-    data["studentId"] = this.studentId;
+
+  Map<String , dynamic> tojson(){
+    final Map<String,dynamic> data = new Map<String , dynamic>();
+    data['StudentId'] = this.studentId;
     data['name'] = this.name;
     data['roll'] = this.roll;
-    data['registrationNo'] = this.registrationNo;
     data['department'] = this.department;
+    data['institution'] = this.institution;
     return data;
   }
 }
